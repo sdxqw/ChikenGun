77297e353f455c16a2fbf8a9c0ffed388ee0e69e
✞ <94248011+sdxqw@users.noreply.github.com>
2025-01-06 21:47:12 +0100

    update

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -8,12 +8,19 @@ public partial class Player : CharacterBody2D
     private const float Speed = 300.0f;
 
     [Export] private HealthComponent HealthComponent { get; set; }
+    [Export] private AnimatedSprite2D _animated;
 
     public override void _Ready()
     {
         if (HealthComponent == null)
         {
             GD.PushError("Player does not have a HealthComponent assigned!");
+            return;
+        }
+        
+        if (_animated == null)
+        {
+            GD.PushError("Player does not have an AnimatedSprite2D assigned!");
             return;
         }
 
@@ -29,11 +36,27 @@ public partial class Player : CharacterBody2D
     {
         var velocity = Velocity;
 
-        var direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        if (direction != Vector2.Zero)
-            velocity.X = direction.X * Speed;
+        var direction = Input.GetAxis("MoveLeft", "MoveRight");
+        if (direction != 0)
+        {
+            velocity.X = direction * Speed;
+            _animated.Play("run");
+        }
         else
+        {
             velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            _animated.Play("idle");
+        }
+    
+        // Flip the sprite based on velocity and mouse position
+        if (velocity.X != 0)
+        {
+            _animated.FlipH = velocity.X < 0;
+        }
+        else
+        {
+            _animated.FlipH = GetGlobalMousePosition().X < GlobalPosition.X;
+        }
 
         Velocity = velocity;
         MoveAndSlide();
